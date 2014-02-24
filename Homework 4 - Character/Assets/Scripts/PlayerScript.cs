c9585fd774acc9c6cbc8d5c03dd59668d32d02a7
Man Chu <arrraichu@gmail.com>
2014-02-23 21:36:18 -0500

    added homework 4

---
+++ Homework 4 - Character/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	// constants used to control the speed of movements, turns, jumps, etc.
+	const float SPEED = 1.5f;
+	const float FORWARD_FORCE = 2f;
+	const float JUMP = 2000f;
+	const float TURN_SPEED = 20f;
+
+	// controls messages to be passed to the game console
+	int nextmessageptr = 0;
+	string[] textbuffer = { 
+		"Hello, and welcome to my world!", 
+		"You can move your character with the WASD controls.", 
+		"You can also turn yourself left and right with [ Q ] or [ E ].", 
+		"Use [ Space ] to jump.", 
+		"In this game, you must collect all spheres!",
+		"Press [ P ] on a sphere to collect it.",
+		"There are two spheres in total.", 
+		"Good Luck!",  
+		"" };
+	float[] textwaittimes = { 0.7f, 5f, 5f, 5f, 7f, 7f, 7f, 5f, 1000f };
+	float timeElapsed = 0f;
+	
+	// movement controllers
+	string movement = "";
+	string turn = "";
+	
+	// game complete checks
+	int collected = 0;
+	bool gamecomplete = false;
+
+	// object referencing the game text console
+	public GameObject textconsole;
+	TextRollingScript textscript;
+
+	// Use this for initialization
+	void Start () {
+		textscript = textconsole.GetComponent<TextRollingScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		controlWASD();
+		controlRotation();
+		if (!Input.anyKey) {
+			rigidbody.velocity = Vector3.zero;
+		}
+		controlCollection();
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rigidbody.AddForce(transform.up * JUMP);
+		}
+		if (collected == 2) {
+			if (!gamecomplete) {
+				textscript.input_text = "Congratulations! You have collected all the spheres.";
+			}
+			gamecomplete = true;
+		}
+		else if (nextmessageptr < textbuffer.Length) {
+			timeElapsed += Time.deltaTime;
+			if (timeElapsed > textwaittimes[nextmessageptr]) {
+				textscript.input_text = textbuffer[nextmessageptr++];
+				timeElapsed = 0f;
+			}
+		}
+		else {
+			textscript.input_text = "";
+		}
+	}
+
+	void controlWASD() {
+		/* detect downs and record them into a string */
+		if (Input.GetKeyDown(KeyCode.W)) {
+			movement = "forward";
+		}
+		else if (Input.GetKeyDown(KeyCode.A)) {
+			movement = "left";
+		}
+		else if (Input.GetKeyDown(KeyCode.S)) {
+			movement = "backward";
+		}
+		else if (Input.GetKeyDown(KeyCode.D)) {
+			movement = "right";
+		}
+
+		/* removes movements, even if multiple keys were fired */
+		if ((Input.GetKeyUp(KeyCode.W) && movement == "forward") || (Input.GetKeyUp(KeyCode.A) && movement == "left") || (Input.GetKeyUp(KeyCode.S) && movement == "backward") || (Input.GetKeyUp(KeyCode.D) && movement == "right")) {
+			movement = "";
+		}
+		
+		/* use the string to control the player capsule */
+		if (movement == "");
+		else if (movement == "forward") {
+			transform.position += transform.forward * SPEED * Time.deltaTime;
+			rigidbody.AddForce(transform.forward * FORWARD_FORCE);
+		}
+		else if (movement == "backward") {
+			transform.position -= transform.forward * SPEED * Time.deltaTime;
+		}
+		else if (movement == "right") {
+			transform.position += transform.right * SPEED * Time.deltaTime;
+		}
+		else if (movement == "left") {
+			transform.position -= transform.right * SPEED * Time.deltaTime;
+		}
+	}
+
+	void controlRotation() {
+		if (Input.GetKeyUp(KeyCode.Q) || Input.GetKeyUp(KeyCode.E)) {
+			turn = "";
+		}
+		else if (Input.GetKeyDown(KeyCode.Q)) {
+			turn = "left";
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			turn = "right";
+		}
+
+		if (turn == "");
+		else if (turn == "left") {
+			transform.Rotate(-1f * transform.up * Time.deltaTime * TURN_SPEED);
+		}
+		else if (turn == "right") {
+			transform.Rotate(transform.up * Time.deltaTime * TURN_SPEED);
+		}
+	}
+
+	void controlCollection() {
+		if (Input.GetKeyDown(KeyCode.P)) {
+				Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
+				for (int i = 0; i < colliders.Length; ++i) {
+					if (colliders[i].GetComponent<SphereCollider>() != null && colliders[i].GetComponent<MeshRenderer>().enabled) {
+						colliders[i].GetComponent<MeshRenderer>().enabled = false;
+							collected++;
+							textscript.input_text = "Total spheres collected: " + collected + " of 2.";
+						break;
+					}
+				}
+		}
+	}
+}
---
+++ Homework 4 - Character/Assets/Scripts/TextRollingScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextRollingScript : MonoBehaviour {
+	string textbuffer = ""; // a buffer to store text that hasn't yet been rolled to the screen
+	float timeElapsed = 0f;
+
+	const float TEXT_ROLLING_SPEED = 0.025f;
+
+	public string input_text = null;
+	bool ready_to_receive_output = true;
+
+	int num_chars = 0;
+	const int NUMCHARS_PER_LINE = 25;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ready_to_receive_output && input_text != null) {
+			textbuffer = input_text;
+			GetComponent<TextMesh>().text = "";
+			input_text = null;
+			timeElapsed = 0f;
+			ready_to_receive_output = false;
+			num_chars = 0;
+		}
+
+		if (!ready_to_receive_output) {
+			if (textbuffer.Length == 0) {
+				ready_to_receive_output = true;
+			}
+			else {
+				timeElapsed += Time.deltaTime;
+				if (timeElapsed > TEXT_ROLLING_SPEED) {
+					timeElapsed = 0;
+					if (++num_chars >= NUMCHARS_PER_LINE && textbuffer[0] == ' ') {
+						GetComponent<TextMesh>().text += "\n";
+						num_chars = 0;
+					}
+					else {
+						GetComponent<TextMesh>().text += textbuffer[0];
+					}
+					textbuffer = textbuffer.Substring(1);
+				}
+			}
+		}
+	}
+}
